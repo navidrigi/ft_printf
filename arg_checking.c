be08b1ef467b2cd30e1fdc5f20c59ca7a57b5ecc
Navid Rigi Ebrahimi <136786793+navidrigi@users.noreply.github.com>
2023-12-31 17:12:24 +0100

    Update arg_checking.c

--- arg_checking.c
+++ arg_checking.c
@@ -1,18 +1,18 @@
 #include "ft_printf.h"
 
-void	arg_checking(const char *str, va_list args, int *len)
+void	arg_checking(char c, va_list args, int *len)
 {
-	if (*str == 'c')
+	if (c == 'c')
 		ft_putchar(va_arg(args, int), len);
-	else if (*str == 's')
+	else if (c == 's')
 		ft_putstr(va_arg(args, char *), len);
-	else if (*str == 'p')
+	else if (c == 'p')
 		ft_puthex((unsigned long)va_arg(args, void *), len);
-	else if (*str == 'd' || *str == 'i')
+	else if (c == 'd' || c == 'i')
 		ft_putnbr(va_arg(args, int), len);
-	else if (*str == 'u')
+	else if (c == 'u')
 		ft_putnbr_u(va_arg(args, unsigned int), len);
-	else if (*str == 'x' || *str == 'X')
+	else if (c == 'x' || c == 'X')
 		ft_puthex(va_arg(args, unsigned int), len);
 	else
 		ft_putchar('%', len);
