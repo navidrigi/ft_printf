758ad9e707cb5587c4901fe715e48ec44af6221f
Navid Rigi Ebrahimi <136786793+navidrigi@users.noreply.github.com>
2023-12-31 17:13:44 +0100

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -23,25 +23,3 @@ int	ft_printf(const char *str, ...)
 	va_end(args);
 	return (len);
 }
-
-char	*ft_strdup(const char *s1)
-{
-	char	*dest;
-	int		s1_len;
-	int		i;
-
-	s1_len = 0;
-	while (s1[s1_len])
-		s1_len++;
-	dest = (char *)malloc(sizeof(char) * (s1_len + 1));
-	if (!dest)
-		return (NULL);
-	i = 0;
-	while (i < s1_len)
-	{
-		dest[i] = s1[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
