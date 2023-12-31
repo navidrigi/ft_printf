736cdda320df76c977a633e36ffcce07eb7c7aa2
Navid Rigi Ebrahimi <136786793+navidrigi@users.noreply.github.com>
2023-12-31 17:14:45 +0100

    Update utils_2.c

--- utils_2.c
+++ utils_2.c
@@ -1,3 +1,23 @@
+#include "ft_printf.h"
+
+void	arg_checking(char c, va_list args, int *len)
+{
+	if (c == 'c')
+		ft_putchar(va_arg(args, int), len);
+	else if (c == 's')
+		ft_putstr(va_arg(args, char *), len);
+	else if (c == 'p')
+		ft_puthex((unsigned long)va_arg(args, void *), len);
+	else if (c == 'd' || c == 'i')
+		ft_putnbr(va_arg(args, int), len);
+	else if (c == 'u')
+		ft_putnbr_u(va_arg(args, unsigned int), len);
+	else if (c == 'x' || c == 'X')
+		ft_puthex(va_arg(args, unsigned int), len);
+	else
+		ft_putchar('%', len);
+}
+
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
